044c86520a24d7a356062182d125d21ac71d1252
Alexandra <55894317+alexandraDem@users.noreply.github.com>
2023-04-13 22:26:56 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,5 +5,6 @@ import numpy as np
 chat_id = 433719125 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y:np.array) -> bool:
+  from scipy.stats import ttest_ind
   pvalue = ttest_ind(x,y,equal_var=False).pvalue
   return True if pvalue < 0.03 else False
