31fe597a51476b5216e8566460a6193b0308feb0
Alexandra <55894317+alexandraDem@users.noreply.github.com>
2023-04-13 22:26:15 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,8 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 433719125 # Ваш chat ID, не меняйте название переменной
 
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+def solution(x: np.array, y:np.array) -> bool:
+  pvalue = ttest_ind(x,y,equal_var=False).pvalue
+  return True if pvalue < 0.03 else False
